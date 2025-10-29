c39fe1a0722d6ad2bd99ddf1f8e8dd61dfeceac2
Mgreat01 <ephraimmonga5@gmail.com>
2025-10-29 14:05:11 +0100

    refactoring de la page

--- lib/pages/cours_page.dart
+++ lib/pages/cours_page.dart
@@ -6,7 +6,9 @@ class CoursPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Page Cours")),
+      body: Center(
+        child: Text("Eza koya .........")
+        ),
     );
   }
 }
